221ebe0a15b939e978481e7eb81c8f8cc805f2c6
Dinesh Bajgain <bajgaindinesh@gmail.com>
2021-10-23 16:14:04 +0545

    simple component api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
-    Job Portal
+    <p>{{name}}</p>
+    <p>{{age}}</p>
+    <button @click="changeName('Dinesh')">Change Name</button>
+    <button @click="changeAge('100')">Change Age</button>
   </div>
 </template>
 
@@ -11,5 +14,20 @@ export default defineComponent({
   name: 'Home',
   components: {
   },
+  data(){
+    return{
+      name:'Link',
+      age:25 as number| string 
+    }
+  },
+  methods:{
+    changeName(name: string,){
+      this.name = name
+      return name
+    },
+    changeAge(age: number|string){
+      this.age = age
+    }
+  }
 });
 </script>
