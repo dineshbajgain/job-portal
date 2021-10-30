a7818ebe11feb47ec5ee797355ef946b1f12e316
Dinesh Bajgain <bajgaindinesh@gmail.com>
2021-10-30 14:33:22 +0545

    tailwind css added

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,5 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import "./assets/global.css"
+import "./assets/tailwind.css"
 createApp(App).use(store).use(router).mount('#app')
